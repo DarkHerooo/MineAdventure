4d2f9f2c04a1caa84319a418167994ad0c4dc82a
Артём Коршунов <korshun.herooo@yandex.ru>
2021-04-08 21:06:37 +0300

    Добавление события KeyDown у формы. Реализация передвижения игрока нажатиями клавиш ВВЕРХ, ВНИЗ, ВЛЕВО, ВПРАВО.

--- MineAdventure/CaveForm.cs
+++ MineAdventure/CaveForm.cs
@@ -19,7 +19,29 @@ namespace MineAdventure
 
         private void CaveForm_KeyDown(object sender, KeyEventArgs e)
         {
+            int xPlayer = pbPlayer.Location.X;
+            int yPlayer = pbPlayer.Location.Y;
+            switch (e.KeyValue)
+            {
+                case (char)Keys.Up:
+                    if (yPlayer > 50)
+                        yPlayer -= 50;
+                    break;
 
+                case (char)Keys.Down:
+                    if (yPlayer < this.Size.Height - 100)
+                        yPlayer += 50;
+                    break;
+
+                case (char)Keys.Left:
+                    if (xPlayer > 50)
+                        xPlayer -= 50;
+                    break;
+                case (char)Keys.Right:
+                    if (xPlayer < this.Size.Width - 100)
+                        xPlayer += 50;
+                    break;
+            }
         }
     }
 }
