113f2bac8866f1a2fdb35fa91e906883dd97f068
Артём Коршунов <korshun.herooo@yandex.ru>
2021-04-13 11:30:24 +0300

    Изменение размеров формы, фона. Размещение картинки игрока.

--- MineAdventure/PlayerForm.Designer.cs
+++ MineAdventure/PlayerForm.Designer.cs
@@ -28,20 +28,51 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pPlayer = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pPlayer.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pPlayer
+            // 
+            this.pPlayer.BackColor = System.Drawing.Color.Transparent;
+            this.pPlayer.Controls.Add(this.pictureBox1);
+            this.pPlayer.Location = new System.Drawing.Point(50, 50);
+            this.pPlayer.Name = "pPlayer";
+            this.pPlayer.Size = new System.Drawing.Size(300, 50);
+            this.pPlayer.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pictureBox1.Image = global::MineAdventure.Properties.Resources.steve;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
             // 
             // PlayerForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(600, 650);
+            this.BackgroundImage = global::MineAdventure.Properties.Resources.BedrockFloor;
+            this.ClientSize = new System.Drawing.Size(400, 650);
+            this.Controls.Add(this.pPlayer);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "PlayerForm";
             this.Text = "PlayerForm";
+            this.pPlayer.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel pPlayer;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
